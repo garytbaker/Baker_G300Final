de51530abed1884a4f475a5f7b30e59932e6f5f7
garytabker <GeeBake@outlook.com>
2020-11-16 18:51:52 -0500

    fixed small bug with the array for the songs

--- Baker_G300Final/Assets/Scripts/CassetteController.cs
+++ Baker_G300Final/Assets/Scripts/CassetteController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CassetteController : MonoBehaviour
 {
-    public int[] inventory = { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+    public int[] inventory = { 0, -1,-1, -1, -1, -1, -1, -1, -1, -1 };
     public int currentSong = 0;
     public bool needsToSwitch = false;
     public int numberOfCassettes = 0;
@@ -22,61 +22,61 @@ public class CassetteController : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKey("0") && inventory[9] > 0)
+        if(Input.GetKey("0") && inventory[9] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 9;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("1") && inventory[0] > 0)
+        else if (Input.GetKey("1") && inventory[0] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 0;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("2") && inventory[1] > 0)
+        else if (Input.GetKey("2") && inventory[1] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 1;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("3") && inventory[2] > 0)
+        else if (Input.GetKey("3") && inventory[2] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 2;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("4") && inventory[3] > 0)
+        else if (Input.GetKey("4") && inventory[3] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 3;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("5") && inventory[4] > 0)
+        else if (Input.GetKey("5") && inventory[4] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 4;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("6") && inventory[5] > 0)
+        else if (Input.GetKey("6") && inventory[5] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 5;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("7") && inventory[6] > 0)
+        else if (Input.GetKey("7") && inventory[6] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 6;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("8") && inventory[7] > 0)
+        else if (Input.GetKey("8") && inventory[7] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 7;
             songs[inventory[currentSong]].Play();
         }
-        else if (Input.GetKey("9") && inventory[8] > 0)
+        else if (Input.GetKey("9") && inventory[8] >= 0)
         {
             songs[inventory[currentSong]].Stop();
             currentSong = 8;

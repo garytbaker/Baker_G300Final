457dc530e8235cbbec15813bd920104d050fac47
garytabker <GeeBake@outlook.com>
2020-11-16 18:45:46 -0500

    first try at changing the songs

--- Baker_G300Final/Assets/Scripts/CassetteController.cs
+++ Baker_G300Final/Assets/Scripts/CassetteController.cs
@@ -17,5 +17,70 @@ public class CassetteController : MonoBehaviour
     private void Start()
     {
         songs[inventory[0]].Play();
+        currentSong = inventory[0];
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("0") && inventory[9] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 9;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("1") && inventory[0] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 0;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("2") && inventory[1] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 1;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("3") && inventory[2] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 2;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("4") && inventory[3] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 3;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("5") && inventory[4] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 4;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("6") && inventory[5] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 5;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("7") && inventory[6] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 6;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("8") && inventory[7] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 7;
+            songs[inventory[currentSong]].Play();
+        }
+        else if (Input.GetKey("9") && inventory[8] > 0)
+        {
+            songs[inventory[currentSong]].Stop();
+            currentSong = 8;
+            songs[inventory[currentSong]].Play();
+        }
     }
 }

54e2749a8c0e29f57840831cd6f3f8610f04a9a0
garytabker <GeeBake@outlook.com>
2020-11-16 20:19:26 -0500

    cleaning up code part two

--- Baker_G300Final/Assets/Scripts/CassetteData.cs
+++ Baker_G300Final/Assets/Scripts/CassetteData.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class CassetteData : MonoBehaviour
 {
-    public int cassetteNumber = 0;
+    public int cassetteNumber = 0;  //the data for which song the cassette holds
 }
--- Baker_G300Final/Assets/Scripts/PlayerMovement.cs
+++ Baker_G300Final/Assets/Scripts/PlayerMovement.cs
@@ -4,35 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public float moveSpeed = 1f;  //the move speed of the player
 
-    public Rigidbody2D playerRigidBody;
+    public Rigidbody2D playerRigidBody;  //the players rigidbody
 
-    Vector2 movement;
+    Vector2 movement;  //the movement the player needs
 
-    public CassetteController cassetteController;
+    public CassetteController cassetteController;  //reference to teh cassette controller
   
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.x = Input.GetAxisRaw("Horizontal");  //getting input from the player
         movement.y = Input.GetAxisRaw("Vertical");
-        movement.Normalize();
+        movement.Normalize();                           //normalizing it so the player goes the same speed in all directions
     }
 
     private void FixedUpdate()
     {
-        playerRigidBody.MovePosition(playerRigidBody.position + movement * moveSpeed * Time.deltaTime);
+        playerRigidBody.MovePosition(playerRigidBody.position + movement * moveSpeed * Time.deltaTime); //moves the player based speed and input
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print(collision.tag);
-       if (collision.tag == "Cassette")
+       if (collision.tag == "Cassette")                 //if the player collides with a cassette
         {
-            cassetteController.addCassette(collision.gameObject.GetComponent<CassetteData>().cassetteNumber);
-            collision.gameObject.SetActive(false);
-            print("collided");
+            cassetteController.addCassette(collision.gameObject.GetComponent<CassetteData>().cassetteNumber);  //add the cassette to the inventory
+            collision.gameObject.SetActive(false); //remove the cassette from the scene
         }
     }
 }

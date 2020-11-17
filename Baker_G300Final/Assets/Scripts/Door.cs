7c916e25b322dca5412d1a15cb623c8215933a62
garytabker <GeeBake@outlook.com>
2020-11-16 19:32:35 -0500

    working on logic for the doors

---
+++ Baker_G300Final/Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public AudioSource openSong;
+    public Vector2 newLocation;
+    void Update()
+    {
+        if(openSong.isPlaying)
+        {
+            moveToNewLocation();
+        }
+    }
+
+    void moveToNewLocation()
+    {
+
+    }
+}

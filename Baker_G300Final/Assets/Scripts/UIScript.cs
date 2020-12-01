b3dfdb8dfeaab86dc51bd93d84c81a87126877c9
garytabker <GeeBake@outlook.com>
2020-11-30 20:21:33 -0500

    working on function that will be called when unlocking the new cassettes

--- Baker_G300Final/Assets/Scripts/UIScript.cs
+++ Baker_G300Final/Assets/Scripts/UIScript.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
+    public Image image0;
+    public Image image1;
+    public Image image2;
+    public Image image3;
+    public Image image4;
+    public Image image5;
+    public Image image6;
+    public Image image7;
+    public Image image8;
+    public Image image9;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +29,9 @@ public class UIScript : MonoBehaviour
 
     public void unlockCassette(int cassetteNumber)
     {
-
+        if(cassetteNumber == 0)
+        {
+            image0.gameObject.SetActive(true);
+        }
     }
 }

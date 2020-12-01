db81d34a373b9d23332185748944c95e70e2be34
garytabker <GeeBake@outlook.com>
2020-12-01 11:28:20 -0500

    made PlayerMovement more legible

--- Baker_G300Final/Assets/Scripts/PlayerMovement.cs
+++ Baker_G300Final/Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");  //getting input from the player
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();                           //normalizing it so the player goes the same speed in all directions
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space"))  //press space to go to next scene for testing purposes will remove later
         {
             print("here");
             SceneManager.LoadScene(1);
@@ -35,23 +35,23 @@ public class PlayerMovement : MonoBehaviour
        if (collision.tag == "Cassette")                 //if the player collides with a cassette
         {
             CassetteController.CC.addCassette(collision.gameObject.GetComponent<CassetteData>().cassetteNumber);  //add the cassette to the inventory
-            collision.gameObject.transform.GetChild(1).gameObject.SetActive(true);
-            collision.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            collision.gameObject.transform.GetChild(1).gameObject.SetActive(true);//turn on the collected text
+            collision.gameObject.transform.GetChild(0).gameObject.SetActive(false);//turn off the cassette sprite
             
-            StartCoroutine(objectDeactivator(collision.gameObject));
+            StartCoroutine(objectDeactivator(collision.gameObject));//tun off the text after one sec using a coroutine
         }
        if(collision.tag == "Orb")
         {
-            CassetteController.CC.addOrb(collision.gameObject.GetComponent<OrbData>().orbNumber);
-            collision.gameObject.transform.GetChild(1).gameObject.SetActive(true);
-            collision.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            StartCoroutine(objectDeactivator(collision.gameObject));
+            CassetteController.CC.addOrb(collision.gameObject.GetComponent<OrbData>().orbNumber);  //add the orb to the inventory
+            collision.gameObject.transform.GetChild(1).gameObject.SetActive(true);//turn on the collected text
+            collision.gameObject.transform.GetChild(0).gameObject.SetActive(false);//turn off the orb sprite
+            StartCoroutine(objectDeactivator(collision.gameObject)); //turn off the collected text after one second using coroutine
         }
     }
 
-    IEnumerator objectDeactivator(GameObject cassette)
+    IEnumerator objectDeactivator(GameObject obj)  //this is a coroutine that will wait one second the set the active of the object to off
     {
-        yield return new WaitForSeconds(1);
-        cassette.SetActive(false); //remove the cassette from the scene
+        yield return new WaitForSeconds(1); //wait one second
+        obj.SetActive(false); //remove the object from the scene
     }
 }

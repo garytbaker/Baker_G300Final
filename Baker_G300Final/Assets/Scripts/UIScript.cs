65e0cd711431962915c3ebf2080de72a4ac1d7fd
garytabker <GeeBake@outlook.com>
2020-12-01 11:18:24 -0500

    made UIScript code more legible

--- Baker_G300Final/Assets/Scripts/UIScript.cs
+++ Baker_G300Final/Assets/Scripts/UIScript.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
-    public Image[] icons;
+    public Image[] icons;   //the icons on the UI indicating the songs
 
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject); //persistent through level changes
     }
 
 
     public void unlockCassette(int cassetteNumber)
     {
-        icons[cassetteNumber].gameObject.SetActive(true);
+        icons[cassetteNumber].gameObject.SetActive(true);  //this puts the icon on the hotbar
     }
 
     public void highlightSong( int cassetteNumber)
     {
-        for(int i =0; i < 10; i++) 
+        for(int i =0; i < 10; i++) //for every icon
         {
-            icons[i].color = new Color(.7f,.7f,.7f,1);
+            icons[i].color = new Color(.7f,.7f,.7f,1);//set its color to grey
         }
-        icons[cassetteNumber].color = Color.white;
+        icons[cassetteNumber].color = Color.white; //then highlight the one that is playing by turning it white
     }
 }

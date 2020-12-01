09361f68a2907c5b34ee68fef83929c2e10499da
garytabker <GeeBake@outlook.com>
2020-12-01 11:14:47 -0500

    working on Portal World UI - making it appear and dissapear take 2

--- Baker_G300Final/Assets/Scripts/Portal.cs
+++ Baker_G300Final/Assets/Scripts/Portal.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)  
     {
         if (collision.gameObject.tag == "Player")
         {

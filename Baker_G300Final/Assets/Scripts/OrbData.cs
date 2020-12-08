fff48d9966628e325d35a72aed5549d439e4bff2
garytabker <GeeBake@outlook.com>
2020-12-08 16:10:23 -0500

    fixed orb bug

--- Baker_G300Final/Assets/Scripts/OrbData.cs
+++ Baker_G300Final/Assets/Scripts/OrbData.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class OrbData : MonoBehaviour
 {
     public int orbNumber = 0;  //the number of the orb
+
+    private void Start()
+    {
+        if (CassetteController.CC.orbs[orbNumber] == true) //if the orb has been collected
+        {
+            gameObject.SetActive(false); //do not respawn
+        }
+    }
 }

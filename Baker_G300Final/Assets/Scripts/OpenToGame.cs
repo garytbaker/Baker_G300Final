3835f693b8fc0f147dbca205231eabc4b1451633
garytabker <GeeBake@outlook.com>
2020-12-08 19:01:24 -0500

    can go from opening to main game now

--- Baker_G300Final/Assets/Scripts/OpenToGame.cs
+++ Baker_G300Final/Assets/Scripts/OpenToGame.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpenToGame : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class OpenToGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
